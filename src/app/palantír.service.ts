993a27dcd29cd7ccdb6700d098694dcff5b7de3c
Silvester <s.g.delarie@gmail.com>
2019-08-12 20:07:24 +0200

    Update palantír.service.ts
    added 2 blocks thijs gave me, errors out though

--- src/app/palantír.service.ts
+++ src/app/palantír.service.ts
@@ -1,4 +1,7 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHandler } from '@angular/common/http';
+import { Observable, BehaviorSubject, Subscription } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -6,6 +9,27 @@ import { Injectable } from '@angular/core';
 export class PalantírService {
 
   constructor() { }
+	
+	getDraft(id: string): Promise<any> {
+    const params = new HttpParams().set('id', id);
+    return new Promise((resolve, reject) => {
+      this.httpClient.get(`${this.draftApiURLs.RETRIEVE}`, { params }).subscribe(
+        (draft) => {
+          resolve(draft);
+        }, (error) => {
+          HttpErrorUtil.handleExpiredSession(error);
+          reject(error);
+        }
+      );
+    });
+  }
+	
+	async resumeDraft(id: string): Promise<void> {
+    try {
+      const draft: SafetyForm = await this.draftHttp.getDraft(id);
+		}
+	}
+	
 }
 
 /**
