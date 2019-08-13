3271c019e15cf7bf21eff767f04ee6aac5efd857
Silvester <s.g.delarie@gmail.com>
2019-08-13 07:13:01 +0200

    custom month + date

--- src/app/personal-banner/personal-banner.component.ts
+++ src/app/personal-banner/personal-banner.component.ts
@@ -13,11 +13,21 @@ export class PersonalBannerComponent implements OnInit {
 	characterName:string;
 	characterFaction:string;
 	time = new Date();
+	date:any;
+	month:any;
+	year:any;
+	frontier12day1:number = 29;
+	frontier12day2:number = 30;
+	frontier12day3:number = 1;
+	frontier12month1:number = 11;
+	frontier12month2:number = 12;
+	frontier12year:number = 2019;
 
   constructor(private palantirService: PalantírService) { }
 
   ngOnInit() {
 		this.characterPersonification();
+		this.timetest();
 		setInterval(() => {
        this.time = new Date();
     }, 1000);
@@ -28,6 +38,22 @@ export class PersonalBannerComponent implements OnInit {
 		this.characacterRank = this.characterInformation.rank;
 		this.characterName = this.characterInformation.character_name;
 		this.characterFaction = this.characterInformation.faction;
+		console.log(this.time);
+	}
+	timetest() {
+		this.date = this.time.getDate();
+		this.month = this.time.getMonth() +1;
+		this.year = this.time.getFullYear();
+		if(this.year == this.frontier12year){
+			console.log('Its 2019');
+			console.log(this.month);
+			if(this.month == this.frontier12month1 || this.month == this.frontier12month2){
+				console.log('its november or december');
+				
+			}
+		}else{
+			console.log('Outside Scope');
+		}
 	}
 
 }
