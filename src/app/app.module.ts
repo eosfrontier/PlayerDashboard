65c32f486d977ca28e91237805653d7a60880412
Silvester <s.g.delarie@gmail.com>
2019-08-18 14:37:35 +0200

    clean up
    removing unused elements

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { NgPipesModule } from 'ng-pipes';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
