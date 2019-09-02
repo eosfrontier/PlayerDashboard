5627e24eb01ecc48eac5fe1a6659225cdd15b84c
Silvester <s.g.delarie@gmail.com>
2019-09-02 13:48:35 +0200

    for safekeeping

---
+++ src/assets/id.php
@@ -0,0 +1,17 @@
+<?php 
+
+	define( '_JEXEC', 1 );
+	define( 'JPATH_BASE', $_SERVER[ 'DOCUMENT_ROOT' ] );
+	
+	// Required Files
+	require_once (JPATH_BASE . '/includes/defines.php');
+	require_once (JPATH_BASE . '/includes/framework.php');
+	
+	// To use Joomla's Database Class
+	
+	$app = JFactory::getApplication('site');
+	$user = JFactory::getUser();
+
+	echo json_encode($user->get('id'));
+	
+?>
