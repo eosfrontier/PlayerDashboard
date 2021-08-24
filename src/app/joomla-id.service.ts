aeeffb4f306d7e16da112bd1ff7f06a36bc54121
SulpherStær <s.g.delarie@gmail.com>
2021-08-24 22:31:31 +0200

    update internal API
    internal API handling changed, less dependant on environment.ts

--- src/app/joomla-id.service.ts
+++ src/app/joomla-id.service.ts
@@ -1,8 +1,8 @@
-import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
-import { catchError, map } from 'rxjs/operators';
-import { environment } from 'src/environments/environment';
+import { HttpClient } from '@angular/common/http'
+import { Injectable } from '@angular/core'
+import { Observable } from 'rxjs'
+import { catchError, map } from 'rxjs/operators'
+import { environment } from 'src/environments/environment'
 
 @Injectable({
   providedIn: 'root',
@@ -10,7 +10,6 @@ import { environment } from 'src/environments/environment';
 export class JoomlaIDService {
   readonly ENV = environment
   readonly idAPI = this.ENV.API.ID
-  readonly nameAPI = this.ENV.API.CHARACTER
   readonly tokenAPI = this.ENV.API.TOKEN
   constructor(private http: HttpClient) {}
 
@@ -19,7 +18,7 @@ export class JoomlaIDService {
       map((result: any) => {
         return result
       }),
-      catchError(error => {
+      catchError((error) => {
         return 'service unavailable'
       }),
     )
--- src/app/palantír.service.ts
+++ src/app/palantír.service.ts
@@ -1,34 +1,35 @@
-import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
-import { catchError, map } from 'rxjs/operators';
-import { environment } from 'src/environments/environment';
+import { HttpClient } from '@angular/common/http'
+import { Injectable } from '@angular/core'
+import { Observable } from 'rxjs'
+import { catchError, map } from 'rxjs/operators'
+import { environment } from 'src/environments/environment'
 
 @Injectable({
   providedIn: 'root',
 })
 export class PalantírService {
   readonly ENV = environment
-  readonly nameAPI = this.ENV.API.CHARACTER
-  readonly skillAPI = this.ENV.API.SKILLS
+  readonly nameAPI = this.ENV.API.ORTHANC + 'chars_all/'
+  readonly metaAPI = this.nameAPI + 'meta/'
+  readonly skillAPI = this.nameAPI + 'skills/'
   readonly tokenAPI = this.ENV.API.TOKEN
-  readonly metaAPI = this.ENV.API.META
+
   eosICTime: any
   readonly eosICTimeAPI = this.ENV.API.TIME
 
   constructor(private http: HttpClient) {}
 
   getSkillsFromAPI(id: string): Promise<any> {
-    const body = {
+    const header = {
       token: this.tokenAPI,
       id: id,
     }
     return new Promise((resolve, reject) => {
-      this.http.post(this.skillAPI, JSON.stringify(body)).subscribe(
-        skills => {
+      this.http.get(this.skillAPI, { headers: header }).subscribe(
+        (skills) => {
           resolve(skills)
         },
-        error => {
+        (error) => {
           //reject(error)
         },
       )
@@ -36,17 +37,17 @@ export class PalantírService {
   }
 
   getPersonFromAPI(id: string): Promise<any> {
-    const body = {
+    const header = {
       token: this.tokenAPI,
       accountID: id,
     }
 
     return new Promise((resolve, reject) => {
-      this.http.post(this.nameAPI, JSON.stringify(body)).subscribe(
-        person => {
+      this.http.get(this.nameAPI, { headers: header }).subscribe(
+        (person) => {
           resolve(person)
         },
-        error => {
+        (error) => {
           //reject(error)
         },
       )
@@ -54,16 +55,16 @@ export class PalantírService {
   }
 
   getMetaFromAPI(id: string): Promise<any> {
-    const body = {
+    const header = {
       token: this.tokenAPI,
       id: id,
     }
     return new Promise((resolve, reject) => {
-      this.http.post(this.metaAPI, JSON.stringify(body)).subscribe(
-        meta => {
+      this.http.get(this.metaAPI, { headers: header }).subscribe(
+        (meta) => {
           resolve(meta)
         },
-        error => {
+        (error) => {
           //reject(error)
         },
       )
@@ -75,7 +76,7 @@ export class PalantírService {
       map((result: any) => {
         return result
       }),
-      catchError(error => {
+      catchError((error) => {
         return error
       }),
     )
